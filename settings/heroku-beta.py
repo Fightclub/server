26f3865032aa19156f7478f22b4e3fd8ca94d741
Peter Tsoi <petertsoi@gmail.com>
2012-10-24 18:35:29 -0700

    Update heroku-beta database settings

--- settings/heroku-beta.py
+++ settings/heroku-beta.py
@@ -3,9 +3,9 @@ from common import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'df8et1an8esk4v',
-        'USER': 'zlajkrzpakqsiy',
-        'PASSWORD': 'psVJSIiTIIyOtRXsKTivzTE0X6',
+        'NAME': 'd59cpdk1qsjkf8',
+        'USER': 'pfjfmwqrzxikun',
+        'PASSWORD': 'O7Lfw11LCwXI8xWYOujOJpYkJ0',
         'HOST': 'ec2-54-243-48-107.compute-1.amazonaws.com',
         'PORT': '5432',
     }
