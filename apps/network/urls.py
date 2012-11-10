93385c2c69bc5bba49faa09516455c5c2a5475fe
Peter Tsoi <petertsoi@gmail.com>
2012-11-09 23:03:02 -0800

    Password hashing and user login

--- apps/network/urls.py
+++ apps/network/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^a/user/new/?$', 'apps.network.views.NewUserJson'),
+    url(r'^a/user/login?$', 'apps.network.views.UserLoginJson'),
 )
--- apps/network/views/User.py
+++ apps/network/views/User.py
@@ -18,7 +18,8 @@ def NewUserJson(request):
     if not DJUser.objects.filter(email=email):
       if not fbemail or not User.objects.filter(fbemail=fbemail):
         try:
-          newDJUser = DJUser(username=email, first_name=first, last_name=last, email=email, password=password)
+          newDJUser = DJUser(username=email, first_name=first, last_name=last, email=email)
+          newDJUser.set_password(password)
           newDJUser.save()
           newUser = User.objects.get(user=newDJUser)
           newUser.bday = bday
@@ -34,3 +35,18 @@ def NewUserJson(request):
       userInfo = {"error": "Email already used"}
   return HttpResponse(json.dumps(userInfo))
 
+def UserLoginJson(request):
+  data = request.GET
+  email = data.get("email", None)
+  fbemail = data.get("fbemail", None)
+  password = data.get("password", None)
+  response = {"error": "Invalid username/password"}
+
+  if password and (email or fbemail):
+    if email:
+      user = User.select(email=email)
+    else:
+      user = User.select(fbemail=fbemail)
+    if user and user.user.check_password(password):
+      response = user.to_dict()
+  return HttpResponse(json.dumps(response))
