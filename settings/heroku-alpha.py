53284116424e594beb7edb195b6886df26793b49
Peter Tsoi <petertsoi@gmail.com>
2012-11-13 14:08:56 -0800

    Update alpha settings

--- settings/heroku-alpha.py
+++ settings/heroku-alpha.py
@@ -3,10 +3,10 @@ from common import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd8bbr0ov3fr9ep',
-        'USER': 'textzbumnnfhkb',
-        'PASSWORD': 'VrYVTPFAR2ukUacETdZcs1_ajC',
-        'HOST': 'ec2-54-243-39-42.compute-1.amazonaws.com',
+        'NAME': 'd5s3vnqth0vcmt',
+        'USER': 'hvneordlmflzgi',
+        'PASSWORD': 'YbCKO6wqkeFpnGCnDou2QR_TZV',
+        'HOST': 'ec2-23-23-144-111.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
